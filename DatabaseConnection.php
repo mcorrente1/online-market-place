7433c7c50629a9521694a6039939ae271a1603d4
mattcorrente <mcorrente72@gmail.com>
2017-06-11 00:06:28 -0700

    organized database functionality into DatabaseConnection class

---
+++ DatabaseConnection.php
@@ -0,0 +1,90 @@
+<?php
+
+class DatabaseConnection {
+
+    private $servername;
+    private $username;
+    private $password;
+    private $dbname;
+    private $conn;
+
+    # constructor defines connection to clearDB on Bluemix
+    function __construct(){
+        $this->servername = "us-cdbr-sl-dfw-01.cleardb.net";
+        $this->username = "b8e24747489532";
+        $this->password = "1a2a9e6a";
+        $this->dbname = "ibmx_fe83c219896edb4";
+
+        $this->connect();
+    }
+
+    # constructor defines connection to clearDB on Bluemix
+    function __destruct(){
+        $this->disconnect();
+    }
+
+    function getServername(){
+        return $this->servername;
+    }
+
+    function getUsername(){
+        return $this->username;
+    }
+
+    function getPassword(){
+        return $this->password;
+    }
+
+    function getDbname(){
+        return $this->dbname;
+    }
+
+    function getConnection(){
+        return $this->conn;
+    }
+
+    function setConnection($connection){
+         $this->conn = $connection;
+    }
+
+
+    function connect(){
+        // check if connection has already been made, if not then make connection
+        if(!$this->getConnection()){
+            $this->setConnection(new mysqli($this->getServername(), $this->getUsername(), $this->getPassword(), $this->getDbname()));
+
+            // Check connection
+            if ($this->conn->connect_error) {
+                die("Connection failed: " . $this->conn->connect_error);
+            }
+            else {
+                echo "Successfully connected to database <br>";
+            }
+        }
+    }
+
+    function disconnect(){
+        $this->getConnection()->close();
+    }
+
+
+    # receives an SQL command, processes the command through the $conn variable then returns the result
+    function queryDB($sql){
+
+        $result = $this->getConnection()->query($sql);
+
+        if ($result->num_rows > 0) {
+            // output data of each row
+         while($row = $result->fetch_assoc()) {
+                echo "id: " . $row["num"]. " - Name: " . $row["string"].  "<br>";
+            }
+        }
+        else {
+            echo "0 results";
+        }
+
+        return $result;
+    }
+}
+
+?>
