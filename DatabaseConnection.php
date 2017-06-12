da107b59ae3071b3b87393dedd7eadd2e11a35cc
mattcorrente <mcorrente72@gmail.com>
2017-06-12 10:14:55 -0700

    updated databaseconnection class

--- DatabaseConnection.php
+++ DatabaseConnection.php
@@ -73,15 +73,15 @@ class DatabaseConnection {
 
         $result = $this->getConnection()->query($sql);
 
-        if ($result->num_rows > 0) {
+        #if ($result->num_rows > 0) {
             // output data of each row
-         while($row = $result->fetch_assoc()) {
-                echo "id: " . $row["num"]. " - Name: " . $row["string"].  "<br>";
-            }
-        }
-        else {
-            echo "0 results";
-        }
+        # while($row = $result->fetch_assoc()) {
+        #        echo "id: " . $row["num"]. " - Name: " . $row["string"].  "<br>";
+        #    }
+        #}
+        #else {
+        #    echo "0 results";
+        #}
 
         return $result;
     }
