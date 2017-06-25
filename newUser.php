38c6a835a44af9fdb8b58002a0abc87f48947854
mattcorrente <mcorrente72@gmail.com>
2017-06-24 17:55:48 -0700

    updated how header is printed

--- newUser.php
+++ newUser.php
@@ -1,28 +1,13 @@
-<!DOCTYPE html>
-<html>
-<head>
-
-	<link rel='stylesheet' type='text/css' href='./website.css'/>
-	<title>Rogue Soda - New User</title>
-	<link rel="icon" href="http://i.imgur.com/9EVFXmq.png">
-</head>
-
-<body>
-	<div id='container'>
-		<div id='header'>
-			<a align='right' href="newUser.php">Sign Up</a> <a> | </a> <a href="login.php">Login</a>
-			<h2 align='center'>New User</h2>
-      <img align='right' src='http://i.imgur.com/9EVFXmq.png' alt = 'Rogue Soda Logo' width=25% height=1%>
-			<h2 style='text-align:center;'></h2>
-
-
-				<a href= "home.php" class="nav_button nav_buttonA">Home</a>
-				<a href="about.php" class="nav_button nav_buttonA">About</a>
-				<a href="contact.php" class="nav_button nav_buttonA">Contact Us</a>
-				<a href= "index.php" class="nav_button nav_buttonA">Store</a>
-			</div>
-
 <?php
+
+require_once("layout.php");
+require_once("Customer.php");
+
+echo $header;
+
+if(!isset($_SESSION)) {
+    session_start();
+}
 
     if(!isset($_SESSION)) {
         session_start();
@@ -146,3 +131,7 @@
     </form>
 </body>
 </html>
+
+<?php
+echo $footer;
+
